82b5e372c3087e51177c25d7f605f6015689ed8c
justanotherlamer <god_of_cf@rambler.ru>
2016-08-19 12:08:33 +0300

    fibonacci instead of ones

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -24,10 +25,26 @@ namespace ConsoleApplication1
 
         static IEnumerable<int> Sequence(int inp)
         {
-            for (int i = 0; i < inp; i++)
+            if(inp <= 0) yield break;
+
+            if (inp >= 1)
             {
                 yield return 1;
-            } 
+                if (inp >= 2)
+                {
+                    yield return 1;
+                    int a = 1;
+                    int b = 1;
+                    for (int i = 3; i <= inp; i++)
+                    {
+                        int cur = a + b;
+                        a = b;
+                        b = cur;
+                        yield return cur;
+
+                    }
+                }
+            }
         }
     }
 }
